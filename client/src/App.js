2d94fab0e6c1008b48175987ba421bc1672b9a54
ythosa <vasus714@yandex.ru>
2020-07-01 17:14:08 +0300

    loader component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from 'react'
+import { Loader } from './components/Loader/Loader'
 
 function App() {
+    const { login, logout, token, userId, ready } = useAuth()
+    const isAuthenticated = !!token
+    const routes = useRoutes(isAuthenticated)
+
+    if (!ready) {
+        return <Loader />
+    }
+
     return (
-        <div>Hi</div>
+        <AuthContext.Provider value={{
+            login, logout, token, userId, isAuthenticated
+        }}>
+            <Router>
+                <div className="container">
+                    { isAuthenticated ? <Navbar /> : null }
+                    { routes }
+                </div>
+            </Router>
+        </AuthContext.Provider>
     )
 }
 
 
-export default App;
+export default App
---
+++ client/src/components/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => {
+    return (
+        <div className="uk-flex uk-flex-center">
+            <span className="uk-margin-small-right" uk-spinner="ratio: 2.5"/>
+        </div>
+    )
+}
