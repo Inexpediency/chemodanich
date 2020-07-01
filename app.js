3c8a7e4e6d8a0a7c14fb1b2b1ea246356fc5f682
ythosa <vasus714@yandex.ru>
2020-07-01 16:43:35 +0300

    client base

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express()
 
 app.use(express.json({ extended: true }))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = config.get('port') || 5000
 const MONGOURI = config.get('mongoUri')
 
